a0c3c0aa42de87addd8f2ed0ae75704093d7c80c
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-05-24 18:43:55 +0200

    [add] search bar

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,28 +1,37 @@
 <header>
-    <nav class="navbar navbar-custom navbar-expand-lg">
-        <div class="container-fluid d-flex justify-content-between">
-            <div class="collapse navbar-collapse">
+    <nav class="navbar-custom">
+        <div class="container-fluid d-flex justify-content-between align-items-center h-100">
 
-                {{-- Left-side --}}
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item d-flex align-items-center me-2">
-                        <i class="fa-solid fa-globe"></i>
-                        <a class="nav-link" href="{{ route('home') }}" target="_blank">Go to website</a>
-                    </li>
-                </ul>
-
-                {{-- Right-side --}}
-                <div class="d-flex justify-content-between align-items-center px-2">
-                    <div class="mx-3"><i class="fa-solid fa-user-shield"></i> {{ Auth::user()->name }}</div>
-
-                    <form action="{{ route('logout') }}" method="POST">
-                        @csrf
-                        <button type="submit" class="btn btn-custom-primary">
-                            <i class="fa-solid fa-right-from-bracket"></i>
-                        </button>
-                    </form>
+            {{-- Left-side --}}
+            <div class="d-flex align-items-center justify-content-between">
+                <div class="d-flex align-items-center ">
+                    <i class="fa-solid fa-globe"></i>
+                    <a class="ms-2" href="{{ route('home') }}" target="_blank">Go to website</a>
                 </div>
             </div>
+
+            {{-- Search --}}
+            <form class="d-flex align-items-center" role="search" action="{{ route('admin.projects.index') }}"
+                method="GET">
+                <input class="form-control form-control-custom me-2" type="search" placeholder="Search a project"
+                    aria-label="Search" name="toSearch">
+                <button class="btn btn-custom-primary-rev" type="submit">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </form>
+
+            {{-- Right-side --}}
+            <div class="d-flex justify-content-between align-items-center px-2">
+                <div class="mx-3"><i class="fa-solid fa-user-shield"></i> {{ Auth::user()->name }}</div>
+
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button type="submit" class="btn btn-custom-primary">
+                        <i class="fa-solid fa-right-from-bracket"></i>
+                    </button>
+                </form>
+            </div>
+
         </div>
     </nav>
 </header>
